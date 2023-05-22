d5c30c6872d0e88b057d7a86a84db3980b2bff79
Ross McEwen <ross.mcewen@gmail.com>
2023-05-22 17:08:27 -0500

    trying to import languages

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,9 @@
 <script>
  import { page } from '$app/stores'
  import { wordSetStore } from '../../stores/wordSet';
+ // @ts-ignore
+ import languages from '$lib/languages.json'
+ wordSetStore.set(languages[$page.params.slug])
 </script>
 
 {$page.params.slug}
