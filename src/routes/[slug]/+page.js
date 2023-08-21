0d59f25c29591f174b8bffdc9b203f04d7961e55
Ross McEwen <ross.mcewen@gmail.com>
2023-08-21 16:06:57 -0500

    udpate route generator with new json path

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,5 +1,5 @@
 /** @type {import('./$types').EntryGenerator} */
-import langSets from '../../../static/languages.json' 
+import langSets from '../../../static/languages/list.json' 
 export async function entries() {
   const langs = Object.keys(langSets)
   return langs.map(lang => ({slug: lang}))
